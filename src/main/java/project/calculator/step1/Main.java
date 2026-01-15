48a45c51bf83e82b38026679d12159dd4abe8667
hyun2ee2 <Hongkobi04@gmail.com>
2026-01-15 18:20:33 +0900

    feat: 스캐너 생성 및 사용자로부터 값을 입력 받는 로직 추가

--- src/main/java/project/calculator/step1/Main.java
+++ src/main/java/project/calculator/step1/Main.java
@@ -1,7 +1,22 @@
 package project.calculator.step1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        // 스캐너 생성
+        Scanner number = new Scanner(System.in);
+        Scanner operator = new Scanner(System.in);
+
+        // 사용자로부터 값 입력 받기
+        System.out.print("첫번째 값을 입력해주세요. : ");
+        int inputNumberOne = number.nextInt();
+
+        System.out.print("연산자를 입력해주세요. : ");
+        String inputOperator = operator.nextLine();
+
+        System.out.print("두번째 값을 입력해주세요. : ");
+        int inputNumberTwo = number.nextInt();
     }
 }
