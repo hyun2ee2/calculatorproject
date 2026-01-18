9a9f905f023e2165ee6c8b54c202f61cbf68c318
hyun2ee2 <Hongkobi04@gmail.com>
2026-01-18 21:19:25 +0900

    feat: Enum Operator 타입 추가

--- src/main/java/project/calculator/step3/OperatorType.java
+++ src/main/java/project/calculator/step3/OperatorType.java
@@ -1,4 +1,27 @@
 package project.calculator.step3;
 
 public enum OperatorType {
+    ADD('+'),
+
+    SUB('-'),
+
+    MUL('*'),
+
+    DIV('/');
+
+    private final char symbol;
+
+    // 생성자 만들기
+    OperatorType(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public static OperatorType fromChar(char operator) {
+        for(OperatorType op : values()) {
+            if(op.symbol == operator) {
+                return op;
+            }
+        }
+        throw new IllegalArgumentException("지원하지 않는 연산자 입니다.");
+    }
 }
