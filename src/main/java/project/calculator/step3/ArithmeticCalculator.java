c575c338aebd60e0c21c7d8c2e170f1f9c3d83bb
hyun2ee2 <Hongkobi04@gmail.com>
2026-01-18 22:17:57 +0900

    feat: 특정 값 보다 큰 결과를 조회할 수 있는 로직 추가

--- src/main/java/project/calculator/step3/ArithmeticCalculator.java
+++ src/main/java/project/calculator/step3/ArithmeticCalculator.java
@@ -1,6 +1,7 @@
 package project.calculator.step3;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArithmeticCalculator {
     private final List<Double> results = new ArrayList<>();
@@ -22,6 +23,13 @@ public class ArithmeticCalculator {
 
     public void removeResults() {
         results.remove(0);
+    }
+
+    // 특정 값 보다 큰 결과 조회 (람다&스트림 활용)
+    public List<Double> getResultsGreaterThan(double threshold) {
+        return results.stream()
+                .filter(v -> v > threshold)
+                .collect(Collectors.toList());
     }
 }
 
--- src/main/java/project/calculator/step3/Main.java
+++ src/main/java/project/calculator/step3/Main.java
@@ -14,8 +14,7 @@ public class Main {
         // 스캐너 생성
         Scanner number = new Scanner(System.in);
         Scanner operator = new Scanner(System.in);
-        Scanner over = new Scanner(System.in);
-        Scanner answer = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // 사용자로부터 값 입력 받기 : 반복
         while (true) {
@@ -38,18 +37,27 @@ public class Main {
             // 저장된 결과 가져오기 Getter
             System.out.println("현재 저장된 결과 : " + calculator.getResults());
 
-            System.out.println("가장 먼저 저장된 결과를 삭제하시겠습니까? (예 : Y입력ㅣ아니오: 아무 키 입력) :");
-            String inputAnswer = answer.next();
+            System.out.print("가장 먼저 저장된 결과를 삭제하시겠습니까? (예 : Y ㅣ 아니오: 아무 키 입력) : ");
+            String inputAnswer = sc.next();
             if (inputAnswer.equalsIgnoreCase("Y")) {
                 calculator.removeResults();
                 System.out.println("가장 먼저 저장된 결과 삭제 후 : " + calculator.getResults());
+            }
+
+            // 특정 값 보다 큰 결과를 조회하는 로직
+            System.out.print("특정 값보다 큰 결과를 조회하시겠습니까? (예 : Y ㅣ 아니오: 아무 키 입력) : ");
+            String query = sc.next();
+            if(query.equalsIgnoreCase("Y")) {
+                System.out.print("기준 값을 입력하세요 : ");
+                double threshold = sc.nextDouble();
+                System.out.println("기준보다 큰 결과들 : " + calculator.getResultsGreaterThan(threshold));
             }
 
 
             // 계속 계산을 진행할건지 사용자에게 물어봄 : 반복
             while (true) {
                 System.out.print("계산을 계속 진행하시겠습니까? (Y: 계속ㅣEXIT: 나가기) : ");
-                String inputOver = over.nextLine();
+                String inputOver = sc.next().trim();
 
                 if (inputOver.equalsIgnoreCase("Y")) {
                     System.out.println("------------------------------------------------------------");
@@ -71,7 +79,7 @@ public class Main {
         // 스캐너 닫기
         number.close();
         operator.close();
-        over.close();
+        sc.close();
     }
 }
 
